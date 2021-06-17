75aa87ede043773ab256054dae604ec4c57babed
lyyre <lyyre@outlook.com>
2021-06-17 14:12:19 +0800

    解决了跨域问题，配置了axios，对接了login接口

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 module.exports = {
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  transpileDependencies: [
+    'vuetify'
+  ], devServer: {
+    open: process.platform === 'darwin',
+    host: '127.0.0.1',
+    port: 8080,
+    proxy: { //配置跨域
+      '/': {
+          //target: 'http://127.0.0.1:9999/api', //这里后台的地址模拟的;应该填写你们真实的后台接口
+           target: 'http://localhost:8081', //这里后台的地址模拟的;应该填写你们真实的后台接口
+
+          ws: true,
+          changOrigin: true, //允许跨域
+          pathRewrite: {
+              '^/': '' //请求的时候使用这个api就可以
+          },
+      }
+  },
+    https: false,
+    hotOnly: false,
+  },
 }
 const webpack = require('webpack')
 
